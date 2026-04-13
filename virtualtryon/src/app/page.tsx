03ff3d166afc9d26aade716a0eaf4febebacabf9
dev13company <avinash.iitg48@gmail.com>
2026-04-13 19:21:15 +0530

    commit

--- virtualtryon/src/app/page.tsx
+++ virtualtryon/src/app/page.tsx
@@ -1,8 +1,17 @@
 export const dynamic = "force-dynamic"
 
+import Hero from "./components/Hero"
+import Categories from "./components/Categories"
+import Products from "./components/Products"
+import Gallery from "./components/Gallery"
+
 export default function Home() {
   return (
     <main className="bg-[#F8F5F2] text-black">
+      <Hero />
+      <Categories/>
+      <Products />
+      <Gallery />
     </main>
   )
 }
