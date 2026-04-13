2711105a8539c414fa65418c4316c45180e01a5f
dev13company <avinash.iitg48@gmail.com>
2026-04-13 22:07:40 +0530

    commit

--- src/sanity/schemaTypes/index.ts
+++ src/sanity/schemaTypes/index.ts
@@ -1,12 +1,7 @@
 import { type SchemaTypeDefinition } from 'sanity'
-import heroSection from './heroSection';
-import galleryImage from './galleryImage';
-import meetingSection from './meetingSection';
-import aboutUsSection from "./aboutUsSection";
-import testimonialSection from './testimonialSection';
 import gallery from './gallery';
 import product from './product';
 
-export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [heroSection, galleryImage, meetingSection, aboutUsSection, testimonialSection, product, gallery],
+export const schematypes: { types: SchemaTypeDefinition[] } = {
+  types: [product, gallery],
 }
